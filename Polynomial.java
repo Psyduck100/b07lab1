3096d6e8452135c2fedce68249fbc51262dd8b56
Rogers Dai <roger.dai.78@gmail.com>
2024-09-20 21:56:48 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,39 @@
+public class Polynomial {
+    private double[] coefficients;
+
+    public Polynomial() {
+        this.coefficients = new double[]{0};
+    }
+
+    public Polynomial(double[] coefficients) {
+        // Copy the input array to avoid mutation from the outside
+        this.coefficients = coefficients.clone();
+    }
+
+    public Polynomial add(Polynomial other) {
+        // Get the degree of the two polynomials
+        int maxDegree = Math.max(this.coefficients.length, other.coefficients.length);
+        double[] resultCoefficients = new double[maxDegree];
+
+        for (int i = 0; i < maxDegree; i++) {
+            double thisCoeff = (i < this.coefficients.length) ? this.coefficients[i] : 0;
+            double otherCoeff = (i < other.coefficients.length) ? other.coefficients[i] : 0;
+            resultCoefficients[i] = thisCoeff + otherCoeff;
+        }
+
+        return new Polynomial(resultCoefficients);
+    }
+
+    public double evaluate(double x) {
+        double result = 0;
+        for (int i = coefficients.length - 1; i >= 0; i--) {
+            result = result * x + coefficients[i];
+        }
+        return result;
+    }
+
+    public boolean hasRoot(double x) {
+        return evaluate(x) == 0;
+    }
+
+}
